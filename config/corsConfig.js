a00b479cb0a25511e67c068c0a598275cec53b1d
Danielzxccc <araojodaniel321@gmail.com>
2023-01-09 17:26:03 +0800

    fix uri

--- config/corsConfig.js
+++ config/corsConfig.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:5173',
-  'https://playlist-sharing.vercel.app/',
+  'https://playlist-sharing.vercel.app',
   'http://localhost:5500',
   'http://127.0.0.1:5500',
 ]
